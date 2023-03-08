1411adab8294cf7e101ef5c2fd963eb5e25d307e
Jorge Pacheco <je.pacheco@uniandes.edu.co>
2023-03-07 22:47:04 -0500

    #comment: Microservicio de ordenes

---
+++ ms-ordenes/app/broker/controllers/command_controller.py
@@ -0,0 +1,61 @@
+from app.broker.commands.create_order import CreateOrderPayload, CommandCreateOrder
+from app.broker.commands.stock_validate import StockValidaterPayload, CommandStockValidate
+from app.broker.commands.create_route import CreateRoutePayload, CreateRouteValidate
+from app.broker.commands.prepare_product import PrepareProductPayload, CommandPrepareProduct
+from app.broker.dispatcher  import Dispatcher
+
+import pulsar
+from pulsar.schema import *
+
+dispatcher = Dispatcher()
+
+class CommandController:
+
+    def OrderCommandCreator(self, data):
+        topico = 'order-command-create'
+        payload = CreateOrderPayload(
+            product_uuid=str(data['product_uuid']),
+            product_quantity=str(data['product_quantity']),
+            order_type=str(data['order_type']),
+            address=str(data['address'])
+        )
+        
+        comando_integracion = CommandCreateOrder(data=payload)
+        dispatcher._publicar_mensaje(comando_integracion, topico, AvroSchema(CommandCreateOrder))
+
+
+    def StockCommandValidator(self, data):
+        topic = 'product-command-validateStock'
+        payload = StockValidaterPayload(
+            order_uuid=str(data['order_uuid']),
+            product_uuid=str(data['product_uuid']),
+            product_quantity=str(data['product_quantity']),            
+        )
+        
+        comando_integracion = CommandStockValidate(data=payload)
+        dispatcher._publicar_mensaje(comando_integracion, topic, AvroSchema(CommandStockValidate))
+
+
+    def RouteCommandCreate(self, data):
+        topic = 'route-command-create'
+        payload = CreateRoutePayload(
+            order_uuid=str(data['order_uuid']),
+            product_uuid=str(data['product_uuid']),
+            product_quantity=str(data['product_quantity']),            
+            address=str(data['address']),            
+        )
+        
+        comando_integracion = CreateRouteValidate(data=payload)
+        dispatcher._publicar_mensaje(comando_integracion, topic, AvroSchema(CreateRouteValidate))
+
+
+    def ProductCommandPrepare(self, data):
+        topic = 'product-command-prepareProduct'
+        payload = PrepareProductPayload(
+            order_uuid=str(data['order_uuid']),
+            product_uuid=str(data['product_uuid']),
+            product_quantity=str(data['product_quantity']),            
+        )
+        
+        comando_integracion = CommandPrepareProduct(data=payload)
+        dispatcher._publicar_mensaje(comando_integracion, topic, AvroSchema(CommandPrepareProduct))
