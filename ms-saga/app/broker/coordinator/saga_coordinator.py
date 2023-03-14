e6e8116540eadb0ce1562c02050c2a8ba4680175
Elkin Rativa <e.rativa@uniandes.edu.co>
2023-03-14 18:10:18 -0500

    feat: implementacion consumidores saga

--- ms-saga/app/broker/coordinator/saga_coordinator.py
+++ ms-saga/app/broker/coordinator/saga_coordinator.py
@@ -1,33 +1,38 @@
 from app.broker.commands.create_order import CommandCreateOrder
+from app.broker.consumer import suscribirse_a_topico
 
 import pulsar, _pulsar
 from pulsar.schema import *
+import asyncio
 
 client = pulsar.Client('pulsar://localhost:6650')
 
 class Saga_Coordinator:
     
-    def init():
+    def init(self):
+
+        task1 = asyncio.ensure_future(suscribirse_a_topico("events-storefront", "sub-storefront", CommandCreateOrder))
+    
         
-        topics = [
-            {'topic': 'order_command_create', 'subscription': 'sub1', 'schema_type': AvroSchema(CommandCreateOrder)}
-        ]
+        # topics = [
+        #     {'topic': 'events-storefront', 'subscription': 'sub1', 'schema_type': AvroSchema(CommandCreateOrder)}
+        # ]
 
-        consumers = []
-        for topic in topics:
-            consumer = client.subscribe(topic=topic['topic'], subscription_name=topic['subscription'], consumer_type=_pulsar.ConsumerType.Shared, schema=topic['schema_type'])
-            consumers.append(consumer)
+        # consumers = []
+        # for topic in topics:
+        #     consumer = client.subscribe(topic=topic['topic'], subscription_name=topic['subscription'], consumer_type=_pulsar.ConsumerType.Shared, schema=topic['schema_type'])
+        #     consumers.append(consumer)
 
-        while True:
-            for consumer in consumers:
-                msg = consumer.receive()
-                try:
-                    print('=========================================')
-                    print("Mensaje Recibido: '%s'" % msg.topic_name())
-                    print("Data Recibida: '%s'" % msg.value().data)
-                    print('=========================================')
+        # while True:
+        #     for consumer in consumers:
+        #         msg = consumer.receive()
+        #         try:
+        #             print('=========================================')
+        #             print("Mensaje Recibido: '%s'" % msg.topic_name())
+        #             print("Data Recibida: '%s'" % msg.value().data)
+        #             print('=========================================')
 
-                    consumer.acknowledge(msg)
+        #             consumer.acknowledge(msg)
 
-                except:
-                    consumer.negative_acknowledge(msg)
+        #         except:
+        #             consumer.negative_acknowledge(msg)
--- ms-saga/entrypoint.py
+++ ms-saga/entrypoint.py
@@ -1,12 +1,7 @@
 from flask_restful import Api
 from flask_cors import CORS
 from app import create_app
-import pulsar, _pulsar
-from pulsar.schema import *
 import os
-import uuid
-import time
-from app.broker.controllers.command_controller import CommandController
 from app.broker.coordinator.saga_coordinator import Saga_Coordinator
 
 saga_coordinator = Saga_Coordinator()
