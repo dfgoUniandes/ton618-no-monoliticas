de04e561972422ad6fe258fa03440e21f4ff3b0b
Elkin Rativa <e.rativa@uniandes.edu.co>
2023-03-15 22:04:23 -0500

    feat: Integracion rutas a sagas

--- ms-saga/app/broker/controllers/event_controller.py
+++ ms-saga/app/broker/controllers/event_controller.py
@@ -9,6 +9,7 @@ from app.broker.events.product_unavailable import ProductUnavailablePayload, Eve
 from app.broker.events.route_created import RouteCreatedPayload, EventRouteCreated
 from app.broker.events.route_unavailable import RouteUnavailablePayload, EventRouteUnavailable
 from app.broker.events.order_received import OrderReceivedEvent, OrderReceivedPayload
+from app.broker.events.route_compensated import RouteCompensatedPayload, EventRouteCompensated
 
 dispatcher = Dispatcher()
 
@@ -54,22 +55,33 @@ class EventController:
     def RouteCreatedEvent(self, data):
         topic = 'route-event-created'
         payload = RouteCreatedPayload(
-            route_uuid = str(data['route_uuid']),
-            order_uuid = str(data['order_uuid']),
-            product_uuid = str(data['product_uuid']),
-            product_quantity = str(data['product_quantity']),
-            address = str(data['address'])           
+            route_uuid=str(data['route_uuid']),
+            order_uuid=str(data['order_uuid']),
+            product_uuid=str(data['product_uuid']),
+            product_quantity=str(data['product_quantity']),
+            address=str(data['address'])
         )
         comando_integracion = EventRouteCreated(data=payload)
-        dispatcher._publicar_mensaje(comando_integracion, topic, AvroSchema(EventRouteCreated))
+        dispatcher._publicar_mensaje(
+            comando_integracion, topic, AvroSchema(EventRouteCreated))
 
     def RouteUnavailableEvent(self, data):
         topic = 'route-event-unavailable'
         payload = RouteUnavailablePayload(
-            order_uuid = str(data['order_uuid']),
-            product_uuid = str(data['product_uuid']),
-            product_quantity = str(data['product_quantity']),
-            address = str(data['address']),          
+            order_uuid=str(data['order_uuid']),
+            product_uuid=str(data['product_uuid']),
+            product_quantity=str(data['product_quantity']),
+            address=str(data['address']),
         )
         comando_integracion = EventRouteUnavailable(data=payload)
-        dispatcher._publicar_mensaje(comando_integracion, topic, AvroSchema(EventRouteUnavailable))
+        dispatcher._publicar_mensaje(
+            comando_integracion, topic, AvroSchema(EventRouteUnavailable))
+
+    def RouteCompensatedEvent(self, data):
+        topic = 'route-event-compensated'
+        payload = RouteCompensatedPayload(
+            route_uuid=str(data['order_uuid']),
+        )
+        comando_integracion = EventRouteCompensated(data=payload)
+        dispatcher._publicar_mensaje(
+            comando_integracion, topic, AvroSchema(EventRouteCompensated))
--- ms-saga/app/broker/coordinator/saga_coordinator.py
+++ ms-saga/app/broker/coordinator/saga_coordinator.py
@@ -1,5 +1,8 @@
 from app.broker.events.order_received import OrderReceivedEvent
 from app.broker.events.order_started import EventOrderStarted
+from app.broker.events.route_created import EventRouteCreated
+from app.broker.events.route_unavailable import EventRouteUnavailable
+from app.broker.events.route_compensated import EventRouteCompensated
 from app.broker.consumer import suscribirse_a_topico
 from app.saga.saga_ordenes import CoordinadorOrdenes
 import asyncio
@@ -18,6 +21,9 @@ class Saga_Coordinator:
         print('Function INIT')
         task1 = asyncio.ensure_future(suscribirse_a_topico("events-storefront", "sub-storefront-1", OrderReceivedEvent, self.coordinador_ordenes))
         task2 = asyncio.ensure_future(suscribirse_a_topico("events-ordenes", "sub-ordenes-1", EventOrderStarted, self.coordinador_ordenes))
+        task2 = asyncio.ensure_future(suscribirse_a_topico("route-event-created", "sub-rutas-1", EventRouteCreated, self.coordinador_ordenes))
+        task2 = asyncio.ensure_future(suscribirse_a_topico("route-event-unavailable", "sub-rutas-2", EventRouteUnavailable, self.coordinador_ordenes))
+        task2 = asyncio.ensure_future(suscribirse_a_topico("route-event-compensated", "sub-rutas-3", EventRouteCompensated, self.coordinador_ordenes))
         
         tasks.append(task1)
         tasks.append(task2)
