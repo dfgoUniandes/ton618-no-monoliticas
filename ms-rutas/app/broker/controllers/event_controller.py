38775f62675e2171cefabf2f98b1b4e8bb171e2c
oscar cortes <oe.cortes@uniandes.edu.co>
2023-03-15 21:23:30 -0500

    Rutas

--- ms-rutas/app/broker/controllers/event_controller.py
+++ ms-rutas/app/broker/controllers/event_controller.py
@@ -1,34 +1,46 @@
 
-from app.broker.dispatcher  import Dispatcher
+from app.broker.dispatcher import Dispatcher
 import pulsar
 from pulsar.schema import *
 from app.broker.events.route_created import RouteCreatedPayload, EventRouteCreated
 from app.broker.events.route_unavailable import RouteUnavailablePayload, EventRouteUnavailable
-
+from app.broker.events.route_compensated import RouteCompensatedPayload, EventRouteCompensated
 
 dispatcher = Dispatcher()
+
 
 class EventController:
 
     def RouteCreatedEvent(self, data):
         topic = 'route-event-created'
         payload = RouteCreatedPayload(
-            route_uuid = str(data['route_uuid']),
-            order_uuid = str(data['order_uuid']),
-            product_uuid = str(data['product_uuid']),
-            product_quantity = str(data['product_quantity']),
-            address = str(data['address'])           
+            route_uuid=str(data['route_uuid']),
+            order_uuid=str(data['order_uuid']),
+            product_uuid=str(data['product_uuid']),
+            product_quantity=str(data['product_quantity']),
+            address=str(data['address'])
         )
         comando_integracion = EventRouteCreated(data=payload)
-        dispatcher._publicar_mensaje(comando_integracion, topic, AvroSchema(EventRouteCreated))
+        dispatcher._publicar_mensaje(
+            comando_integracion, topic, AvroSchema(EventRouteCreated))
 
     def RouteUnavailableEvent(self, data):
         topic = 'route-event-unavailable'
         payload = RouteUnavailablePayload(
-            order_uuid = str(data['order_uuid']),
-            product_uuid = str(data['product_uuid']),
-            product_quantity = str(data['product_quantity']),
-            address = str(data['address']),          
+            order_uuid=str(data['order_uuid']),
+            product_uuid=str(data['product_uuid']),
+            product_quantity=str(data['product_quantity']),
+            address=str(data['address']),
         )
         comando_integracion = EventRouteUnavailable(data=payload)
-        dispatcher._publicar_mensaje(comando_integracion, topic, AvroSchema(EventRouteUnavailable))
+        dispatcher._publicar_mensaje(
+            comando_integracion, topic, AvroSchema(EventRouteUnavailable))
+
+    def RouteCompensatedEvent(self, data):
+        topic = 'route-event-compensated'
+        payload = RouteCompensatedPayload(
+            route_uuid=str(data['order_uuid']),
+        )
+        comando_integracion = EventRouteCompensated(data=payload)
+        dispatcher._publicar_mensaje(
+            comando_integracion, topic, AvroSchema(EventRouteCompensated))
--- ms-rutas/app/domain/domain.py
+++ ms-rutas/app/domain/domain.py
@@ -1,7 +1,7 @@
+from app.models.models import db, ma, Routes
 from app.broker.controllers.event_controller import EventController
 from app.broker.consumer import suscribirse_a_topico
 import asyncio
-from app.utils.utils import uuid4Str
 from app.broker.commands.create_route import CreateRouteValidate
 tasks = list()
 event_controller = EventController()
@@ -27,16 +27,64 @@ class Domain:
         if command == 'coordinar-ruta':
 
             global event_controller
+            if self.validate_address(data_command):
+                route_id = await self.create_route(data_command)
+                await self.create_success_emit_event(route_id, data_command)
+            else:
+                await self.create_error_emit_event(data_command)
 
-            # Logica al recepcionar el comando
-            # Interaccion con DB
+        if command == 'compensacion-coordinar-ruta':
+            await self.compensate_create_route(data_command)
 
-            data_event_emit = {}
-            data_event_emit['route_uuid'] = uuid4Str()
-            data_event_emit['order_uuid'] = data_command.order_uuid
-            data_event_emit['product_uuid'] = data_command.product_uuid
-            data_event_emit['product_quantity'] = data_command.product_quantity
-            data_event_emit['order_type'] = data_command.order_type
-            data_event_emit['address'] = data_command.address
+    async def validate_address(self, data) -> bool:
+        return data.address != ""
 
-            event_controller.RouteCreatedEvent(data_event_emit)
+    async def create_route(self,  data_command) -> str:
+        try:
+            route = Routes(
+                product_uuid=data_command.product_uuid,
+                product_quantity=data_command.product_quantity,
+                address=data_command.address)
+            db.session.add(route)
+            db.session.commit()
+            return str(route.uuid)
+        except Exception:
+            self.create_error_emit_event(data_command)
+        finally:
+            db.session.close()
+
+    async def delete_route(self, route_id):
+        try:
+            route = Routes.query.filter_by(uuid=route_id).first()
+            db.session.delete(route)
+            db.session.commit()
+        except Exception as e:
+            print(e)
+        finally:
+            db.session.close()
+
+    async def create_success_emit_event(self, route_id, data_command):
+        data_event_emit = {}
+        data_event_emit['route_uuid'] = route_id
+        data_event_emit['order_uuid'] = data_command.order_uuid
+        data_event_emit['product_uuid'] = data_command.product_uuid
+        data_event_emit['product_quantity'] = data_command.product_quantity
+        data_event_emit['order_type'] = data_command.order_type
+        data_event_emit['address'] = data_command.address
+
+        event_controller.RouteCreatedEvent(data_event_emit)
+
+    async def create_error_emit_event(self, data_command):
+        route_unavailable_event = {}
+        route_unavailable_event['order_uuid'] = data_command.order_uuid
+        route_unavailable_event['product_uuid'] = data_command.product_uuid
+        route_unavailable_event['product_quantity'] = data_command.product_quantity
+        route_unavailable_event['address'] = data_command.address
+
+        event_controller.RouteUnavailableEvent(route_unavailable_event)
+
+    async def compensate_create_route(self, data_command):
+        print('Compensacion de la creacion de la ruta')
+        route_compensated_event = {}
+        route_compensated_event['route_uuid'] = data_command.route_uuid
+        event_controller.RouteCompensatedEvent(route_compensated_event)
